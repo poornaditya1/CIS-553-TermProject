76c59d2a5416dcfd91677f3818a6e78dc764e36d
Poornaditya Mishra <42487913+poornaditya1@users.noreply.github.com>
2022-11-15 09:33:56 +0530

    Alternate version

---
+++ RequestTest.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+import requests
+
+url = 'https://docs.google.com/forms/d/e/1FAIpQLScGia1PMCFzaURH1UUX0DXtfzcQ-uyF8kStjZp0cDjccx5Cvw/formResponse?'
+
+def listen(prompt):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # add function for speaking using the "prompt"
+        audio = r.listen(source)
+    text1 = r.recognize_google(audio)
+    text = text1.replace(" ", "+")
+    return text
+
+def fillForm():
+    name = listen("Speak your name")
+    role = listen("Speak your role")
+    uni = listen("Speak the name of your university")
+    data = url + "entry.2109765752=" + name + "&entry.636569773=" + role + "&entry.958710338=" + uni
+    requests.post(data)
+
+if __name__ == "__main__":
+    fillForm()
+    print("Done!")
